c673aefb56cb10b25d162b01b8941e0fe0b87811
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-09-18 12:33:44 -0600

    Updated

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -5,16 +5,16 @@ namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
-      private IEnumerable<IProcessOneRequest> ProcessList;
+      private IEnumerable<IProcessOneRequest> all_commands;
 
       public CommandRegistry(IEnumerable<IProcessOneRequest> processList)
       {
-          ProcessList = processList;
+          all_commands = processList;
       }
 
       public IProcessOneRequest get_the_command_that_can_process(IEncapsulateRequestDetails request)
       {
-          return ProcessList.Single(process => process.can_run(request));
+          return all_commands.Single(process => process.can_run(request));
     }
   }
 }
