ddd21f527c550ad3884d80448239e0aed57e3d7e
vermegi <gitte.vermeiren@gmail.com>
2012-09-18 20:16:41 +0200

    Pushing new changes

--- source/app/web/FrontController.cs
+++ source/app/web/FrontController.cs
@@ -1,10 +1,23 @@
-﻿namespace app.web
+﻿using System.Web.UI.WebControls;
+
+namespace app.web
 {
   public class FrontController : IProcessRequests
   {
-    public void process(IEncapsulateRequestDetails request)
-    {
-      throw new System.NotImplementedException();
-    }
+      private IFindCommands command_finder;
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+      /// </summary>
+      public FrontController(IFindCommands command_finder)
+      {
+          this.command_finder = command_finder;
+      }
+
+      public void process(IEncapsulateRequestDetails request)
+      {
+          var the_command = command_finder.get_the_command_that_can_process(request);
+          the_command.run(request);
+      }
   }
 }
