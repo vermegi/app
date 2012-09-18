ff0a9fb7aea962112d6015564bedc5288b558dc6
ridingwolf <ridingwolf@github.com>
2012-09-18 20:31:43 +0200

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -1,10 +1,20 @@
-﻿namespace app.web
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
-    public IProcessOneRequest get_the_command_that_can_process(IEncapsulateRequestDetails request)
-    {
-      throw new System.NotImplementedException();
+      private IEnumerable<IProcessOneRequest> ProcessList;
+
+      public CommandRegistry(IEnumerable<IProcessOneRequest> processList)
+      {
+          ProcessList = processList;
+      }
+
+      public IProcessOneRequest get_the_command_that_can_process(IEncapsulateRequestDetails request)
+      {
+          return ProcessList.Single(process => process.can_run(request));
     }
   }
 }
