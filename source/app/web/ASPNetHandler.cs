3a4955ba8644578a5e8ca59dfc02b771b7939c02
nruys <niels.ruys@exsertus.be>
2012-09-18 19:52:19 +0200

    Pushing new changes

--- source/app/web/ASPNetHandler.cs
+++ source/app/web/ASPNetHandler.cs
@@ -4,13 +4,22 @@ using System.Web;
 namespace app.web
 {
   public class ASPNetHandler : IHttpHandler
-
   {
+      private IProcessRequests _frontController;
+      private ICreateControllerRequests _requestFactory;
+
+      public ASPNetHandler(IProcessRequests processRequests, ICreateControllerRequests createControllerRequests)
+      {
+          this._frontController = processRequests;
+          this._requestFactory = createControllerRequests;
+      }
+
     public bool IsReusable { get; private set; }
 
     public void ProcessRequest(HttpContext context)
     {
-      throw new NotImplementedException();
+        var request = _requestFactory.create_request_from(context);
+        this._frontController.process(request);
     }
   }
 }
