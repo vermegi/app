79471e4c0069ceb8e41993986f5df92076772c3b
vermegi <gitte.vermeiren@gmail.com>
2012-09-18 20:32:07 +0200

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -1,10 +1,26 @@
-﻿namespace app.web
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
-    public IProcessOneRequest get_the_command_that_can_process(IEncapsulateRequestDetails request)
-    {
-      throw new System.NotImplementedException();
+      private IEnumerable<IProcessOneRequest> possible_commands;
+
+      public CommandRegistry(IEnumerable<IProcessOneRequest> possible_commands)
+      {
+          this.possible_commands = possible_commands;
+      }
+
+      public IProcessOneRequest get_the_command_that_can_process(IEncapsulateRequestDetails request)
+      {
+          foreach (var command in possible_commands)
+          {
+              if (command.can_run(request))
+                  return command;
+          }
+
+          return null;
     }
   }
 }
