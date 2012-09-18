470be1a7d1970e3965387c3904c9d466dedb946d
Werner Vermeylen <werner.vermeylen@exsertus.be>
2012-09-18 16:43:49 +0200

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,10 +1,13 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int i, int i1)
     {
-      return i + i1;
+        if(i < 0 || i1 < 0) throw new ArgumentException("Not allowed to add a negative to a positive.");
+        return i + i1;
     }
   }
 }
