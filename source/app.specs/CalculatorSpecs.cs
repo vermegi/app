f1d7bafcde50eef0415633fad91ab1f4246798b5
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-09-18 08:53:43 -0600

    Updated

--- source/app.specs/CalculatorSpecs.cs
+++ source/app.specs/CalculatorSpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using Machine.Specifications;
 using developwithpassion.specifications.rhinomocks;
 using developwithpassion.specifications.extensions;
@@ -13,8 +14,20 @@ namespace app.specs
 
     public class when_adding_two_numbers : concern
     {
+      //arrange
+      Establish c = () =>
+      {
+        connection = depends.on<IDbConnection>();   
+      };
+
+
+      It should_open_a_connection_To_the_db = () =>
+        connection.received(x => x.Open());
+        
       It should_return_the_sum = () =>
         sut.add(2, 3).ShouldEqual(5);
+
+      static IDbConnection connection;
     }
 
     public class when_attempting_to_add_a_Negative_to_a_positive : concern
