9dfa4d7c3c4173ca1a422f59e8bbd0a205f628c2
vermegi <gitte.vermeiren@gmail.com>
2012-09-18 19:41:16 +0200

    Pushing new changes

--- source/app/web/ASPNetHandler.cs
+++ source/app/web/ASPNetHandler.cs
@@ -6,11 +6,23 @@ namespace app.web
   public class ASPNetHandler : IHttpHandler
 
   {
-    public bool IsReusable { get; private set; }
+      private IProcessRequests processor;
+      private ICreateControllerRequests request_creator;
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+      /// </summary>
+      public ASPNetHandler(IProcessRequests processor, ICreateControllerRequests request_creator)
+      {
+          this.processor = processor;
+          this.request_creator = request_creator;
+      }
+
+      public bool IsReusable { get; private set; }
 
     public void ProcessRequest(HttpContext context)
     {
-      throw new NotImplementedException();
+      processor.process(request_creator.create_request_from(context));
     }
   }
 }
