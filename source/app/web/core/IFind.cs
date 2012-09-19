d5f5989090b0c1066d8f26a558d35c758ce88559
koenmannaerts <koen.mannaerts@gmail.com>
2012-09-19 16:10:52 +0200

    Pushing new changes

---
+++ source/app/web/core/IFind.cs
@@ -0,0 +1,7 @@
+﻿namespace app.web.core
+{
+  public interface IFind
+  {
+    ReportModel find<SpecificRequest, ReportModel>(SpecificRequest request);
+  }
+}
---
+++ source/app/web/core/ViewType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace app.web.core
+{
+  public class ViewType<SpecificRequest, ReportModel> : ISupportAUserFeature
+  {
+    
+    public void run(IEncapsulateRequestDetails request)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
