26ad8a58a075135b869e52a94d209564fea83c83
vermegi <gitte.vermeiren@gmail.com>
2012-09-19 20:00:24 +0200

    Pushing new changes

--- source/app/web/core/aspnet/WebFormDisplayEngine.cs
+++ source/app/web/core/aspnet/WebFormDisplayEngine.cs
@@ -1,17 +1,22 @@
-﻿namespace app.web.core.aspnet
+﻿using System.Web;
+
+namespace app.web.core.aspnet
 {
   public class WebFormDisplayEngine : IDisplayReports
   {
     ICreateDisplayHandlers handler_factory;
+      private HttpContext current_context;
 
-    public WebFormDisplayEngine(ICreateDisplayHandlers handlers_factory)
+    public WebFormDisplayEngine(ICreateDisplayHandlers handlers_factory, HttpContext currentContext)
     {
-      this.handler_factory = handlers_factory;
+        this.handler_factory = handlers_factory;
+        current_context = currentContext;
     }
 
-    public void display<ReportModel>(ReportModel report)
+      public void display<ReportModel>(ReportModel report)
     {
-      handler_factory.create_handler_to_display(report);
+      var handler = handler_factory.create_handler_to_display(report);
+          handler.ProcessRequest(current_context);
     }
   }
 }
