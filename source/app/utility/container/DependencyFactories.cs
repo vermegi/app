b94d522485e58e0ff0b88a22bea26166fc1438d4
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-09-20 08:37:07 -0600

    Updated

--- source/app/utility/container/DependencyFactories.cs
+++ source/app/utility/container/DependencyFactories.cs
@@ -7,21 +7,21 @@ namespace app.utility.container
   public class DependencyFactories : IFindFactoriesForDependencies
   {
     IEnumerable<ICreateOneDependency> possible_factories;
-      private MissingFactory_Behaviour missingFactoryBehaviour;
+    MissingFactory_Behaviour missing_factory_behaviour;
 
-    public DependencyFactories(IEnumerable<ICreateOneDependency> possible_factories, MissingFactory_Behaviour missingFactoryBehaviour)
+    public DependencyFactories(IEnumerable<ICreateOneDependency> possible_factories,
+                               MissingFactory_Behaviour missingFactoryBehaviour)
     {
-        this.possible_factories = possible_factories;
-        this.missingFactoryBehaviour = missingFactoryBehaviour;
+      this.possible_factories = possible_factories;
+      this.missing_factory_behaviour = missingFactoryBehaviour;
     }
 
-      public ICreateOneDependency get_factory_that_can_create(Type dependency)
-      {
-          var factory = possible_factories.FirstOrDefault(x => x.can_create(dependency));
-          if(factory != null)
-            return factory;
+    public ICreateOneDependency get_factory_that_can_create(Type dependency)
+    {
+      var factory = possible_factories.FirstOrDefault(x => x.can_create(dependency));
+      if (factory != null) return factory;
 
-          throw missingFactoryBehaviour(dependency);
-      }
+      throw missing_factory_behaviour(dependency);
+    }
   }
 }
