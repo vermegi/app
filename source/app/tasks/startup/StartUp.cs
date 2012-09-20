5da0dff6aafbe02a0947a76401f97b8b3c9d7ce8
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-09-20 12:01:46 -0600

    Updated

--- source/app/tasks/startup/StartUp.cs
+++ source/app/tasks/startup/StartUp.cs
@@ -14,7 +14,8 @@ namespace app.tasks.startup
     {
       GetTheActiveContainer_Behaviour resolution =
         () =>
-          new Container(new DependencyFactories(all_factories, StartupItems.exception_factories.missing_factory), StartupItems.exception_factories.dependency_creation);
+          new Container(new DependencyFactories(all_factories, StartupItems.exception_factories.missing_factory), 
+            StartupItems.exception_factories.dependency_creation);
 
       Dependencies.container_resolver = resolution;
 
@@ -37,11 +38,12 @@ namespace app.tasks.startup
         public static readonly MissingFactory_Behaviour missing_factory = type =>
         {
           throw new NotImplementedException(string.Format("There is no factory registered that can create a {0}",type.Name));
-        }
-        public static DependencyCreation_Behaviour dependency_creation = (type,inner) =>
+        };
+
+        public static DependencyCreation_Behaviour dependency_creation = (type, inner) =>
         {
-          throw new NotImplementedException(string.Format("There was an error attempting to create a {0}",type.Name),inner);
-        }
+          throw new NotImplementedException(string.Format("There was an error attempting to create a {0}", type.Name), inner);
+        };
         public static MissingCommandCreation_Behaviour missing_command = () =>
         {
           throw new NotImplementedException("You don't have a command that can run this request");
