326ddfb778937ad8f2c5d22ab59dfc9cea921b5a
vermegi <gitte.vermeiren@gmail.com>
2012-09-20 16:19:24 +0200

    Pushing new changes

--- source/app/utility/container/DependencyFactories.cs
+++ source/app/utility/container/DependencyFactories.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace app.utility.container
 {
   public class DependencyFactories: IFindFactoriesForDependencies
   {
-    public ICreateOneDependency get_factory_that_can_create(Type dependency)
+      IEnumerable<ICreateOneDependency> possible_factories;
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+      /// </summary>
+      public DependencyFactories(IEnumerable<ICreateOneDependency> possible_factories)
+      {
+          this.possible_factories = possible_factories;
+      }
+
+      public ICreateOneDependency get_factory_that_can_create(Type dependency)
     {
-      throw new NotImplementedException();
+          foreach (var factory in possible_factories)
+          {
+              if (factory.can_create(dependency))
+                  return factory;
+          }
+
+          throw new ArgumentException("couldn't find the factory.");
     }
   }
 }
