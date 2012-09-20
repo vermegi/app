eabd2e9dab00f6cfae5dd1981db587a2d6dfd7d2
ridingwolf <ridingwolf@github.com>
2012-09-20 16:27:10 +0200

    Pushing new changes

--- source/app/utility/container/DependencyFactories.cs
+++ source/app/utility/container/DependencyFactories.cs
@@ -7,15 +7,21 @@ namespace app.utility.container
   public class DependencyFactories : IFindFactoriesForDependencies
   {
     IEnumerable<ICreateOneDependency> possible_factories;
+      private MissingFactory_Behaviour missingFactoryBehaviour;
 
-    public DependencyFactories(IEnumerable<ICreateOneDependency> possible_factories)
+    public DependencyFactories(IEnumerable<ICreateOneDependency> possible_factories, MissingFactory_Behaviour missingFactoryBehaviour)
     {
-      this.possible_factories = possible_factories;
+        this.possible_factories = possible_factories;
+        this.missingFactoryBehaviour = missingFactoryBehaviour;
     }
 
-    public ICreateOneDependency get_factory_that_can_create(Type dependency)
-    {
-      return possible_factories.First(x => x.can_create(dependency));
-    }
+      public ICreateOneDependency get_factory_that_can_create(Type dependency)
+      {
+          var factory = possible_factories.FirstOrDefault(x => x.can_create(dependency));
+          if(factory != null)
+            return factory;
+
+          throw missingFactoryBehaviour(dependency);
+      }
   }
 }
